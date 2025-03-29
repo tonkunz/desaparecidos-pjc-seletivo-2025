1086c5dc11a03cb53d4c5ebea778fa7b15cb5af9
Everton Kunz <ehokeverton@gmail.com>
2025-03-28 20:44:28 -0400

    Ajusta informações do card de desaparecido

--- src/app/modules/desaparecidos/desaparecidos-list/components/individuo-card/desaparecido-card.component.ts
+++ src/app/modules/desaparecidos/desaparecidos-list/components/individuo-card/desaparecido-card.component.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IDesaparecido } from '../../desaparecidos.types';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'desaparecido-card',
   templateUrl: './desaparecido-card.component.html',
   imports: [
     RouterLink,
+    TitleCasePipe,
+    DatePipe
   ]
 })
 export class DesaparecidoCardComponent {
