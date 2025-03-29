de7ac7891ff68e20c9515b007e9d27fd7b896a25
Everton Kunz <ehokeverton@gmail.com>
2025-03-29 17:56:50 -0400

    Estiliza Header e Footer da Aplicação

--- src/app/shared/components/global-header/global-header.component.ts
+++ src/app/shared/components/global-header/global-header.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'global-header',
   templateUrl: './global-header.component.html',
+  standalone: true,
+  imports: [
+    RouterLink
+  ]
 })
 export class GlobalHeaderComponent { }
